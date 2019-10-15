f0b0b771cbebca1a3ee9ceb436cb91de5935bc44
GauntNight <44542034+GauntNight@users.noreply.github.com>
2019-10-15 21:27:04 +0200

    Create WebScraping.py

---
+++ WebScraping.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 15 20:59:40 2019
+
+@author: rpete
+"""
+
+from bs4 import BeautifulSoup
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+
